70c11220982e9301ffd67f4d0c011d3cd2200d52
DanielIndeed <47148638+DanielIndeed@users.noreply.github.com>
2023-05-05 15:08:35 +0300

    Update Model_trainer.py

--- Main code/Model_trainer.py
+++ Main code/Model_trainer.py
@@ -3,32 +3,25 @@
 
 # In[ ]:
 
-
-#Reading data
-
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split 
 from sklearn.ensemble import RandomForestRegressor
 
-train_data = pd.read_csv(r'D:\My Documents\I3C\Data\Dataset1.csv', delimiter=',') # Read in data
+train_data = pd.read_csv(r'D:\My Documents\I3C\Data\Dataset1.csv', delimiter=',')
 
-labels = np.array(train_data['Earplugs']) # Labels are the values we want to predict
+labels = np.array(train_data['Earplugs'])
 criteria = np.array(train_data['Criteria'])
 
-train_data = train_data.drop('Earplugs', axis = 1) # Remove the labels from the features and axis 1 refers to the columns
+train_data = train_data.drop('Earplugs', axis = 1)
 train_data = train_data.drop('Criteria', axis = 1)
 
-train_data_list = list(train_data.columns) # Saving factor names for later use
-train_data = np.array(train_data) # Convert to numpy array
+train_data_list = list(train_data.columns)
+train_data = np.array(train_data)
 
 
 # In[ ]:
 
-
-#Training data
-
-# Split the data into training and testing sets
 train_train_data, test_train_data, train_labels, test_labels = train_test_split(
     train_data, 
     labels, 
@@ -36,19 +29,18 @@ train_train_data, test_train_data, train_labels, test_labels = train_test_split(
     random_state = 4000,
 )
 
-# Instantiate model with 1000 decision trees
 rf = RandomForestRegressor(
     n_estimators = 1000, 
     random_state = 42
 ) 
 
-rf.fit(train_train_data, train_labels); # Train the model on training data
+rf.fit(train_train_data, train_labels)
 
 
 # In[ ]:
 
 
-predictions = rf.predict(test_train_data) # Use the forest's predict method on the test data
-errors = abs(predictions - test_labels) # Calculate the absolute errors
+predictions = rf.predict(test_train_data)
+errors = abs(predictions - test_labels)
 np.set_printoptions(suppress=True)
 
